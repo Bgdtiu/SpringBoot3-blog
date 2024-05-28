e4b7458ca16c427074a50867d251f6a189567f7a
tfh <3172487926@qq.com>
2024-05-28 20:19:08 +0800

    删除主类Hello World，创建SpringBoot启动类

---
+++ blog/src/main/java/com/tfh/blog/SpringBootMain.java
@@ -0,0 +1,17 @@
+package com.tfh.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tang
+ * @version 1.0
+ * @since 2024/5/28 19:19
+ * TODO: 启动 SpringBoot3 的类
+ */
+@SpringBootApplication
+public class SpringBootMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMain.class, args);
+    }
+}
